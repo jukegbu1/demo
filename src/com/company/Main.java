6498ecdce8b94b8c6b07fd35c9b303f6073b3960
jukegbu1 <jukegbu1@gmail.com>
2019-04-30 19:23:58 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner (System.in);
+
+        System.out.print("Does this work or no");
+        keyboard.next();
+        System.out.println("Yes it does work!");
+
+    }
+}
